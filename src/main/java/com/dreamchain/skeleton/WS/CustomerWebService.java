c20ee0fcbf99b46875a962c60de9754edb66da93
Abdul Kader <babu313136@gmail.com>
2016-11-04 03:06:25 +0600

    Add Customer soap web service

---
+++ src/main/java/com/dreamchain/skeleton/WS/CustomerWebService.java
@@ -0,0 +1,33 @@
+package com.dreamchain.skeleton.WS;
+
+import com.dreamchain.skeleton.model.Customer;
+import com.dreamchain.skeleton.model.User;
+import com.dreamchain.skeleton.service.CustomerService;
+import com.dreamchain.skeleton.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+/**
+ * Created by LAPTOP DREAM on 11/4/2016.
+ */
+
+@WebService
+public class CustomerWebService {
+
+    @Autowired(required = true)
+    private CustomerService customerService;
+
+//    @WebMethod(exclude = true)
+//    public void setCustomerService(CustomerService customerService) {
+//        this.customerService = customerService;
+//    }
+    @WebMethod(operationName = "getCustomerById")
+    public Customer getCustomerById(long id) {
+        Customer customer = customerService.get(id);
+        return customer;
+    }
+
+
+}
